bad91f2e68e024c1a06f3042d89c48130aacc995
Erik Summerfield <e2thex@Erik-Summerfield.local>
2025-03-21 18:14:26 -0500

    more updates getting the page working with the new tooling

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 module.exports = function(eleventyConfig) {
     // Copy assets directly to output
     eleventyConfig.addPassthroughCopy("src/assets");
-    eleventyConfig.addPassthroughCopy("src/tools/ScrumRACI/script.js");
     eleventyConfig.addPassthroughCopy("src/assets/js");
     
     return {
