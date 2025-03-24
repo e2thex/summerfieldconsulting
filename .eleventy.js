4b76a8bdf0ac44304e120d013c9da4e1e87be2df
Erik Summerfield <e2thex@eriksummerfield.lan>
2025-03-23 21:06:27 -0500

    test out a passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
     // Copy assets directly to output
     eleventyConfig.addPassthroughCopy("src/assets");
+    // Add specific passthrough for troubleshooting
+    eleventyConfig.addPassthroughCopy({"src/assets/media/ErikSummerfield.png": "assets/media/ErikSummerfield.png"});
     
     return {
         dir: {
